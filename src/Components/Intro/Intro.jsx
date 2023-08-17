92eb5406681039c4dba2610edf09a07ac0be55f0
Temo Tsintsadze <temo.cincadze@gmail.com>
2023-08-17 13:59:35 +0400

    Add Intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="intro">
-        
+          <div className="i-left">
+            <div className="i-name">
+              <span>Hey' I Am</span>
+              <span>Temo Tsintsadze</span>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio optio suscipit in esse accusantium perferendis.</span>
+            </div>
+            <button className="button i-button">Hire Me</button>
+          </div>
+          <div className="i-right">
+
+          </div>
     </div>
   )
 }
